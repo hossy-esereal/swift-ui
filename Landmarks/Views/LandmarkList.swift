057855e0ceab3c93890ab585a40e8a3e227c70e3
y.hoshi <ysk.hoshi.1118@gmail.com>
2021-01-14 01:35:54 +0900

    add ~section6

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by hossy on 2021/01/13.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) {
+                landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
